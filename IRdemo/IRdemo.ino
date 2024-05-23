317f7a4507406109a4f5bc74322e86d78125f32d
NotAKatfish <timxiong@g.ucla.edu>
2024-05-23 14:10:59 -0700

    LINE FOLLOWING CODE

--- IRdemo/IRdemo.ino
+++ IRdemo/IRdemo.ino
@@ -15,46 +15,46 @@
 QTRSensors qtr;
 
 const uint8_t SensorCount = 8;
-uint16_t sensorValues[SensorCount];
-uint16_t IRValues[SensorCount];
-int s0;
-int s1;
-int s2;
-int s3;
-int s4;
-int s5;
-int s6;
-int s7;
+int sensorValues[SensorCount];
+const int32_t offsetVal[SensorCount] = {449, 227, 197, 180, 183, 247, 160, 373};
+const int32_t normVal[SensorCount] = {525, 712, 734, 749, 747, 699, 774, 587};
+int32_t s[SensorCount];
 
 
 
 int FLpin1 = 22;
 int FLpin2 = 23;
-int FLpinEN = 10;
+int FLpinEN = 13;
 
 int FRpin1 = 24;
 int FRpin2 = 25;
-int FRpinEN = 11;
+int FRpinEN = 12;
 
 int BLpin1 = 28;
 int BLpin2 = 29;
-int BLpinEN = 12;
+int BLpinEN = 11;
 
 int BRpin1 = 26;
 int BRpin2 = 27;
-int BRpinEN = 13;
+int BRpinEN = 10;
 
-const float Nspeed = 60;
+const float Nspeed = 80;
 const float Kp = .2;
 const float Ki = 0.0;
-const float Kd = 0.0;
+const float Kd = 0.2;
 
 int error = 0;
 int prevError = 0;
 int speedLeft = Nspeed;
 int speedRight = Nspeed;
 
-int threshold = 100;
+int up_threshold = 150;
+int low_threshold = -100;
+
+int Lpin1 = 1;
+int Lpin2 = 0;
+int Rpin1 = 1;
+int Rpin2 = 0;
 
 float P = 0;
 float I = 0;
@@ -67,47 +67,16 @@ void setup()
   qtr.setTypeAnalog();
   qtr.setSensorPins((const uint8_t[]){A8, A9, A10, A11, A12, A13, A14, A15}, SensorCount);
   qtr.setEmitterPin(2);
-  delay(500);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
 
- for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr.calibrate();
-  }
-  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
-
-  // print the calibration minimum values measured when emitters were on
-  Serial.begin(9600);
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
   
   pinMode(FLpin1, OUTPUT);
   pinMode(FLpin2, OUTPUT);
   pinMode(FLpinEN, OUTPUT);
 
   
-
-  
   pinMode(FRpin1, OUTPUT);
   pinMode(FRpin2, OUTPUT);
   pinMode(FRpinEN, OUTPUT);
-
-  
 
   pinMode(BLpin1, OUTPUT);
   pinMode(BLpin2, OUTPUT);
@@ -117,17 +86,18 @@ void setup()
   pinMode(BRpin2, OUTPUT);
   pinMode(BRpinEN, OUTPUT);
 
-  digitalWrite(FLpin1, LOW);
-  digitalWrite(FLpin2, HIGH);
-  
-  digitalWrite(FRpin1, LOW);
-  digitalWrite(FRpin2, HIGH);
 
-  digitalWrite(BLpin1, HIGH);
-  digitalWrite(BLpin2, LOW);
+  digitalWrite(FLpin1, Lpin1);
+  digitalWrite(FLpin2, Lpin2);
   
-  digitalWrite(BRpin1, HIGH);
-  digitalWrite(BRpin2, LOW);
+  digitalWrite(FRpin1, Rpin1);
+  digitalWrite(FRpin2, Rpin2);
+
+  digitalWrite(BLpin1, Lpin1);
+  digitalWrite(BLpin2, Lpin2);
+  
+  digitalWrite(BRpin1, Rpin1);
+  digitalWrite(BRpin2, Rpin2);
 
   
   Serial.begin(9600);
@@ -136,15 +106,15 @@ void setup()
 
 void loop()
 {
-//  // read raw sensor values
+  // read raw sensor values
     getError();
     pid = (Kp * P) - (Ki * I) + (Kd * D);
     speedLeft = Nspeed - pid;
     speedRight = Nspeed + pid;
-    if (speedLeft > threshold){ speedLeft = threshold;}
-    if (speedLeft < 0){ speedLeft = 0;}
-    if (speedRight > threshold){ speedRight = threshold;}
-    if (speedRight < 0){ speedRight = 0;}
+    if (speedLeft > up_threshold){speedLeft = up_threshold;}
+    if (speedLeft < low_threshold){ speedLeft = low_threshold;}
+    if (speedRight > up_threshold){ speedRight = up_threshold;}
+    if (speedRight < low_threshold){ speedRight = low_threshold;}
     Serial.print('\t');
     Serial.print(pid);
     Serial.print('\t');
@@ -153,18 +123,51 @@ void loop()
     Serial.print(speedRight);
     Serial.println();
 
-    analogWrite(BRpinEN, speedRight);
-    analogWrite(FLpinEN, speedLeft);
-    analogWrite(BLpinEN, speedLeft);
-    analogWrite(FRpinEN, speedRight);
+    if(speedLeft < 0)
+    {
+      Lpin1 = 0;
+      Lpin2 = 1;
+    } else if(speedLeft >= 0)
+    {
+      Lpin1 = 1;
+      Lpin2 = 0;
+    }
+    if(speedRight < 0)
+    {
+      Rpin1 = 0;
+      Rpin2 = 1;
+    } else if(speedRight >= 0)
+    {
+      Rpin1 = 1;
+      Rpin2 = 0;
+    }
+
+    digitalWrite(FLpin1, Lpin1);
+    digitalWrite(FLpin2, Lpin2);
+  
+    digitalWrite(FRpin1, Rpin1);
+    digitalWrite(FRpin2, Rpin2);
+
+    digitalWrite(BLpin1, Lpin1);
+    digitalWrite(BLpin2, Lpin2);
+  
+    digitalWrite(BRpin1, Rpin1);
+    digitalWrite(BRpin2, Rpin2);
+
+    analogWrite(BRpinEN, abs(speedRight));
+    analogWrite(FLpinEN, abs(speedLeft));
+    analogWrite(BLpinEN, abs(speedLeft));
+    analogWrite(FRpinEN, abs(speedRight));
 
 
   // print the sensor values as numbers from 0 to 1023, where 0 means maximum
   // reflectance and 1023 means minimum reflectance
  
-  //analogWrite(BRpinEN, 100);
-
-  
+//  analogWrite(BRpinEN, 255);
+//  analogWrite(FRpinEN, 255);
+//  analogWrite(BLpinEN, 255);
+//  analogWrite(FLpinEN, 255);
+//  
 }
 
 void getError()
@@ -172,23 +175,21 @@ void getError()
    qtr.read(sensorValues);
    for (uint8_t i = 0; i < SensorCount; i++)
   {
-    //IRValues[i] = sensorValue[i];
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
+    s[i] = (sensorValues[i]-offsetVal[i])*1000/normVal[i];
+    if(s[i] < 0)
+    {
+      s[i] = 0; 
+    }
+//    Serial.print(s[i]);
+//    Serial.print('\t');
   }
-  s0 = sensorValues[0];
-  s1 = sensorValues[1];
-  s2 = sensorValues[2];
-  s3 = sensorValues[3];
-  s4 = sensorValues[4];
-  s5 = sensorValues[5];
-  s6 = sensorValues[6];
-  s7 = sensorValues[7];
-  error = 4*s0+3*s1+2*s2+s3-s4-2*s5-3*s6-4*s7;
+  error = 8*s[0]+4*s[1]+2*s[2]+s[3]-s[4]-2*s[5]-4*s[6]-8*s[7];
+
   
   P = error;
   I = I + error;
   D = error-prevError;
+  Serial.print('\t');
   Serial.print(error);
 //  Serial.print('\t');
 //  Serial.print(P);
